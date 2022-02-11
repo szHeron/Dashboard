0b52464208997ca6b4bd7f5af305d121b293d6c2
szHeron <heronbr7@gmail.com>
2022-02-11 15:28:10 -0300

    added sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
 import HeaderBar from './components/HeaderBar';
 import NavBar from './components/NavBar';
-import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <HeaderBar/>
-      <NavBar/>
+        <HeaderBar/>
+        <NavBar/>
     </BrowserRouter>
   );
 }
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,16 +1,49 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { AiOutlineDashboard, AiOutlineUser, AiOutlineDollar } from "react-icons/ai";
+import { BsListCheck } from "react-icons/bs";
 import './style.css'
 
 export default function NavBar() {
+  const links = [
+    {
+      text: "Dashboard",
+      icon: AiOutlineDashboard,
+      active: true
+    },
+    {
+      text: "Pedidos",
+      icon: BsListCheck,
+      active: false
+    },
+    {
+      text: "Usuarios",
+      icon: AiOutlineUser,
+      active: false
+    },
+    {
+      text: "Transações",
+      icon: AiOutlineDollar,
+      active: false
+    },
+    {
+      text: "Produtos",
+      icon: AiOutlineDashboard,
+      active: false
+    },
+  ]
   return (
     <div className='navbar'>
-      <Link to="/" className='menuBars'>
-        DASHBOARD
-      </Link>
-      <Link to="/" className='menuBars'>
-        USERS
-      </Link>
+      {links.map((link)=>{
+        return(
+          <ul className={'linkBars',link.active&&'actived'}>
+            <a href="#">
+              <link.icon/>
+              {link.text}
+            </a>
+          </ul>
+        )
+      })}
     </div>
   )
 }
