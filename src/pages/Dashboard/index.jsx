3923dfaccd1931f29714ba9c37e713d124d11135
szheron <heronbr7@gmail.com>
2022-03-21 19:47:30 -0300

    update in headerbaar

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -7,14 +7,15 @@ import { AiOutlineDollar } from "react-icons/ai";
 import './style.scss'
 
 export default function Dashboard() {
+  const data = [200, 59000.99, 23000.99, 36000.99];
   return (
     <div className="dashboard">
       <div className='wrapper'/>
       <div className="metrics">
-        <MetricCard Icon={AiOutlineDollar} Name="Vendas" Value={'200 itens'} Color='#00ff00'/>
-        <MetricCard Icon={AiOutlineDollar} Name="Receita" Value={'R$ 59000'} Color='#00ff00'/>
-        <MetricCard Icon={AiOutlineDollar} Name="Despesa" Value={'R$ 23000'} Color='#00ff00'/>
-        <MetricCard Icon={AiOutlineDollar} Name="Total" Value={'R$ 36000'} Color='#00ff00'/>
+        <MetricCard Icon={AiOutlineDollar} Name="Vendas" Value={`${data[0]} itens`} Color='#008000'/>
+        <MetricCard Icon={AiOutlineDollar} Name="Receita" Value={data[1].toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})} Color='#008000'/>
+        <MetricCard Icon={AiOutlineDollar} Name="Despesa" Value={data[2].toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})} Color='#008000'/>
+        <MetricCard Icon={AiOutlineDollar} Name="Total" Value={data[3].toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})} Color='#008000'/>
       </div>
       <div className="charts">
         <Revenue/>
